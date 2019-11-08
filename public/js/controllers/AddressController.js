1ee1d5691eb56a13696f43363bd8d99ef800f8b9
vanpav <vanpav@gmail.com>
2019-11-08 18:40:02 +0300

    BS-255 cleanup

--- public/js/controllers/AddressController.js
+++ public/js/controllers/AddressController.js
@@ -1,30 +1,37 @@
-angular.module('BlocksApp').controller('AddressController', function($stateParams, $rootScope, $scope, $http, $location) {
+angular
+  .module('BlocksApp')
+  .controller('AddressController', function(
+    $stateParams,
+    $rootScope,
+    $scope,
+    $http,
+    $location
+  ) {
     var activeTab = $location.url().split('#');
-    if (activeTab.length > 1)
-      $scope.activeTab = activeTab[1];
+    if (activeTab.length > 1) $scope.activeTab = activeTab[1];
 
-    $rootScope.$state.current.data["pageSubTitle"] = $stateParams.hash;
+    $rootScope.$state.current.data['pageSubTitle'] = $stateParams.hash;
     $scope.addrHash = $stateParams.hash;
-    $scope.addr = {"balance": 0, "count": 0, "mined": 0};
+    $scope.addr = { balance: 0, count: 0, mined: 0 };
     $scope.settings = $rootScope.setup;
 
     //fetch web3 stuff
     $http({
       method: 'POST',
       url: '/web3relay',
-      data: {"addr": $scope.addrHash, "options": ["balance", "count", "bytecode"]}
+      data: { addr: $scope.addrHash, options: ['balance', 'count', 'bytecode'] }
     }).then(function(resp) {
       $scope.addr = $.extend($scope.addr, resp.data);
       fetchTxs();
       if (resp.data.isContract) {
-        $rootScope.$state.current.data["pageTitle"] = "Contract Address";
+        $rootScope.$state.current.data['pageTitle'] = 'Contract Address';
         fetchInternalTxs();
       }
     });
 
     //fetch transactions
     var fetchTxs = function() {
-      var table = $("#table_txs").DataTable({
+      var table = $('#table_txs').DataTable({
         processing: true,
         serverSide: true,
         paging: true,
@@ -35,14 +42,17 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
             // save data
             $scope.data = resp.data;
             // check $scope.records* if available.
-            resp.data.recordsTotal = $scope.recordsTotal ? $scope.recordsTotal : resp.data.recordsTotal;
-            resp.data.recordsFiltered = $scope.recordsFiltered ? $scope.recordsFiltered : resp.data.recordsFiltered;
+            resp.data.recordsTotal = $scope.recordsTotal
+              ? $scope.recordsTotal
+              : resp.data.recordsTotal;
+            resp.data.recordsFiltered = $scope.recordsFiltered
+              ? $scope.recordsFiltered
+              : resp.data.recordsFiltered;
             callback(resp.data);
           });
 
           // get mined, recordsTotal counter only once.
-          if (data.draw > 1)
-            return;
+          if (data.draw > 1) return;
 
           $http.post('/addr_count', data).then(function(resp) {
             $scope.addr.count = resp.data.recordsTotal;
@@ -61,69 +71,77 @@ angular.module('BlocksApp').controller('AddressController', function($stateParam
             }
           });
         },
-        "lengthMenu": [
-                    [10, 20, 50, 100, 150, 500],
-                    [10, 20, 50, 100, 150, 500] // change per page values here
-                ],
-        "pageLength": 20,
-        "order": [
-            [6, "desc"]
+        lengthMenu: [
+          [10, 20, 50, 100, 150, 500],
+          [10, 20, 50, 100, 150, 500] // change per page values here
         ],
-        "language": {
-          "lengthMenu": "_MENU_ transactions",
-          "zeroRecords": "No transactions found",
-          "infoEmpty": ":(",
-          "infoFiltered": "(filtered from _MAX_ total txs)"
+        pageLength: 20,
+        order: [[6, 'desc']],
+        language: {
+          lengthMenu: '_MENU_ transactions',
+          zeroRecords: 'No transactions found',
+          infoEmpty: '',
+          infoFiltered: '(filtered from _MAX_ total txs)'
         },
-        "columnDefs": [
-          { "targets": [ 5 ], "visible": false, "searchable": false },
-          {"type": "date", "targets": 6},
-          {"orderable": false, "targets": [0,2,3,4]},
-          { "render": function(data, type, row) {
-                        if (data != $scope.addrHash)
-                          return '<a href="/addr/'+data+'">'+data+'</a>'
-                        else
-                          return data
-                      }, "targets": [2,3]},
-          { "render": function(data, type, row) {
-                        return '<a href="/block/'+data+'">'+data+'</a>'
-                      }, "targets": [1]},
-          { "render": function(data, type, row) {
-                        return '<a href="/tx/'+data+'">'+data+'</a>'
-                      }, "targets": [0]},
-          { "render": function(data, type, row) {
-                        return getDuration(data).toString();
-                      }, "targets": [6]},
-          ]
+        columnDefs: [
+          { targets: [5], visible: false, searchable: false },
+          { type: 'date', targets: 6 },
+          { orderable: false, targets: [0, 2, 3, 4] },
+          {
+            render: function(data, type, row) {
+              if (data != $scope.addrHash)
+                return '<a href="/addr/' + data + '">' + data + '</a>';
+              else return data;
+            },
+            targets: [2, 3]
+          },
+          {
+            render: function(data, type, row) {
+              return '<a href="/block/' + data + '">' + data + '</a>';
+            },
+            targets: [1]
+          },
+          {
+            render: function(data, type, row) {
+              return '<a href="/tx/' + data + '">' + data + '</a>';
+            },
+            targets: [0]
+          },
+          {
+            render: function(data, type, row) {
+              return getDuration(data).toString();
+            },
+            targets: [6]
+          }
+        ]
       });
-    }
+    };
 
     var fetchInternalTxs = function() {
       $http({
         method: 'POST',
         url: '/web3relay',
-        data: {"addr_trace": $scope.addrHash}
+        data: { addr_trace: $scope.addrHash }
       }).then(function(resp) {
         $scope.internal_transactions = resp.data;
       });
-    }
-
-})
-.directive('contractSource', function($http) {
-  return {
-    restrict: 'E',
-    templateUrl: '/views/contract-source.html',
-    scope: false,
-    link: function(scope, elem, attrs){
+    };
+  })
+  .directive('contractSource', function($http) {
+    return {
+      restrict: 'E',
+      templateUrl: '/views/contract-source.html',
+      scope: false,
+      link: function(scope, elem, attrs) {
         //fetch contract stuff
         $http({
           method: 'POST',
           url: '/compile',
-          data: {"addr": scope.addrHash, "action": "find"}
+          data: { addr: scope.addrHash, action: 'find' }
         }).then(function(resp) {
           // console.log(resp.data);
           scope.contract = resp.data;
         });
       }
-  }
-})
+    };
+  });
